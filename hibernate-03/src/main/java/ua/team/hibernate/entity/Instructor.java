bcd5355307f680f2bf714df506a71d89a61bffad
sfanpro <st.sergeyf@gmail.com>
2019-04-11 17:40:09 +0300

    Created EagerLazyDemo class

--- hibernate-03/src/main/java/ua/team/hibernate/entity/Instructor.java
+++ hibernate-03/src/main/java/ua/team/hibernate/entity/Instructor.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -49,8 +50,8 @@ public class Instructor {
 	@JoinColumn(name = "instructor_detail_id")
 	private InstructorDetail instructorDetail;
 
-	@OneToMany(mappedBy = "instructor", cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
-			CascadeType.PERSIST, CascadeType.REFRESH })
+	@OneToMany(fetch = FetchType.EAGER, mappedBy = "instructor", cascade = { CascadeType.DETACH, CascadeType.MERGE,
+			CascadeType.PERSIST, CascadeType.PERSIST, CascadeType.REFRESH })
 	private List<Course> courses;
 
 	public Instructor() {
