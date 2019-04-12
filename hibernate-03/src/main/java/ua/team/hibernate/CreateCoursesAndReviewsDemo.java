a29b2d4406c51318f381ec8ddb783090aef9ebf9
sfanpro <st.sergeyf@gmail.com>
2019-04-12 12:06:37 +0300

    Create reviews entity

---
+++ hibernate-03/src/main/java/ua/team/hibernate/CreateCoursesAndReviewsDemo.java
@@ -0,0 +1,51 @@
+package ua.team.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import ua.team.hibernate.entity.Course;
+import ua.team.hibernate.entity.Instructor;
+import ua.team.hibernate.entity.InstructorDetail;
+import ua.team.hibernate.entity.Review;
+
+public class CreateCoursesAndReviewsDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class).addAnnotatedClass(Course.class)
+				.addAnnotatedClass(Review.class).buildSessionFactory();
+
+		// create session factory
+		Session session = factory.getCurrentSession();
+		try {
+
+			// start a transaction
+			session.beginTransaction();
+
+			// create a course
+			Course tempCourse = new Course("fasting");
+
+			// add some reviews
+			tempCourse.addReview(new Review("deep knowlage, great course ... loved it!"));
+			tempCourse.addReview(new Review("Cool course, job half done!"));
+			tempCourse.addReview(new Review("What a wise course ... loved it!"));
+
+			// save the course ... and leverage the cascade all
+			System.out.println("Saving the course");
+			System.out.println(tempCourse);
+			System.out.println(tempCourse.getReviews());
+			session.save(tempCourse);
+
+			// commit transaction
+			session.getTransaction().commit();
+			System.out.println("Done!");
+		} finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
